0d3660bbdeaf8de9cea58e33adddfe10cbfb672a
jonecole <jonecole@oregonstate.edu>
2020-01-23 12:32:57 -0800

    Progress made on New Job form; Paper database JSON file added, will eventually be replaced by API call to fetch, but for testing purposes it's staying local; Added radio buttons for paper settings, dynamically scraped from paper database file. Will need to discuss with clients how we want to proceed, since there are some issues with uniques in the database and I'm not sure how we want to go about resolving this.

--- Website/SmartJobAdvisor/ClientApp/src/components/NewJob.js
+++ Website/SmartJobAdvisor/ClientApp/src/components/NewJob.js
@@ -5,11 +5,84 @@ import Style from '../CSS/NewJob.module.css'
 
 const { Option } = Select;
 
+/* Function to convert a string to proper title case. */
+String.prototype.toProperCase = function () {
+	return this.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
+};
+
 class NewJobForm extends React.Component {
-	state = {
-		coverageVal: 50,
+	constructor() {
+		super();
+
+		/* This will eventually be replaced with a call to a database.
+		 * For now, this file exists locally. */
+		var paperDatabase = require('../PaperData/SJA-paper-db-2020-01-20-trim.json');
+
+		this.state = {
+			paperDatabase: paperDatabase.paperdb,
+			coverageVal: 50,
+			paperNameDropdown: this.getDropdown(paperDatabase.paperdb, "productname"),
+			paperWeightRadio: this.getRadio(paperDatabase.paperdb, "weightclass"),
+			paperTypeRadio: this.getRadio(paperDatabase.paperdb, "papertype"),
+			paperSubTypeRadio: this.getRadio(paperDatabase.paperdb, "papersubtype"),
+			paperFinishRadio: this.getRadio(paperDatabase.paperdb, "finish"),
+		};
+
+		this.getRadio(paperDatabase.paperdb, 'manufacturer');
 	};
 
+	getRadio = (data, key) => {
+		// Create the select component
+		var radio = [];
+
+		var tempArray = data.map(function (e) { return e[key]; }).filter((v, i, a) => a.indexOf(v) === i);
+
+		for (let i = 0; i < tempArray.length; i++) {
+			var text = tempArray[i].toProperCase();
+			radio.push(<Radio.Button key={i} value={text}>{text}</Radio.Button>);
+		}
+
+		return radio;
+	}
+
+	getDropdown = (data, key) => {
+		// Create the select component
+		var dropdown = [];
+
+		for (let i = 0; i < data.length; i++) {
+			var text = data[i][key].toProperCase();
+			dropdown.push(<Option key={text}>{text}</Option>);
+		}
+
+		return dropdown;
+
+		/* This is an example of creating the options for a dropdown menu based off
+		 * of data fetched via an API call. Leaving this here as an example.
+		 * 
+		// Fetch the degree table to get values for select component
+		fetch(serverURL + "get_table/degree", {
+			method: "GET",
+			headers: {
+				"Accept": "application/json"
+			}
+		}).then((res) => {
+			res.json().then((data) => {
+				// Create the select component
+				var dropdown = [];
+
+				for (let i = 0; i < data.length; i++) {
+					var text = data[i].degreesID + " - " + data[i].degreeCombination;
+					dropdown.push(<Option key={data[i].degreesID}>{text}</Option>);
+				}
+				this.setState({ degreeDropdown: dropdown })
+			});
+		}).catch(err => err);
+		*/
+	};
+
+
+
+	/* Called when the coverage value is changed. */
 	onCoverageValChange = val => {
 		this.setState({
 			coverageVal: val,
@@ -17,33 +90,24 @@ class NewJobForm extends React.Component {
 	};
 
 	onPaperNameChange = val => {
-		const { setFieldsValue, getFieldValue } = this.props.form;
+		const { setFieldsValue } = this.props.form;
 
-		if (val === "Paper A") {
-			setFieldsValue({
-				paperSurface: "Coated",
-				paperWeight: "Heavy",
-				paperFinish: "Gloss",
-			});
-		}
-		else if (val === "Paper B") {
-			setFieldsValue({
-				paperSurface: "Uncoated",
-				paperWeight: "Light",
-				paperFinish: "Silk",
-			});
-		}
-		else if (val === "Paper C") {
-			setFieldsValue({
-				paperSurface: "Coated",
-				paperWeight: "Medium",
-				paperFinish: "Satin",
-			});
-		}
+		var selectedPaper = this.state.paperDatabase.filter((e) => e.productname === val)[0];
+
+		setFieldsValue({
+			productname: selectedPaper.productname.toProperCase(),
+			manufacturer: selectedPaper.manufacturer.toProperCase(),
+			weightclass: selectedPaper.weightclass.toProperCase(),
+			papertype: selectedPaper.papertype.toProperCase(),
+			papersubtype: selectedPaper.papersubtype.toProperCase(),
+			finish: selectedPaper.finish.toProperCase(),
+			useprimer: selectedPaper.useprimer.toProperCase(),
+			usefixer: selectedPaper.usefixer.toProperCase(),
+		});
 	};
 
 	resetPaperName = () => {
-		const { setFieldsValue, getFieldValue } = this.props.form;
+		const { setFieldsValue } = this.props.form;
 
 		setFieldsValue({
 			paperName: undefined,
@@ -60,13 +124,20 @@ class NewJobForm extends React.Component {
 	};
 
 	render() {
-		const { coverageVal } = this.state;
-		const { getFieldDecorator } = this.props.form;
+		const { coverageVal, paperManufacturer } = this.state;
+		const { getFieldDecorator, getFieldsValue } = this.props.form;
 
 		const paperFormItemLayout = {
-			labelCol: { span: 4 },
-			wrapperCol: { span: 14 },
+			labelCol: { span: 2 },
+			wrapperCol: { span: 22 },
 		}
+
+		const paperFormItemLayoutName = {
+			labelCol: { span: 3 },
+			wrapperCol: { span: 21 },
+		}
+
+		console.log(this.state.paperDatabase);
 
 		return (
 			<div className={Style.newJobFormContainer}>
@@ -81,8 +152,8 @@ class NewJobForm extends React.Component {
 				<Form layout="vertical" onSubmit={this.handleSubmit} className={Style.newJobForm}>
 					<h5>General Info</h5>
 					<Row gutter={12}>
-						<Col span={12}>
-							<Form.Item label="Job Name">
+						<Col span={8}>
+							<Form.Item label="Job Name" style={{ marginBottom: 10 }}>
 								{getFieldDecorator('jobName', {
 									rules: [{ required: true, message: 'Please input a job name' }],
 									initialValue: "Setting Advice",
@@ -93,8 +164,8 @@ class NewJobForm extends React.Component {
 								)}
 							</Form.Item>
 						</Col>
-						<Col span={12}>
-							<Form.Item label="Ruleset">
+						<Col span={7}>
+							<Form.Item label="Ruleset" style={{ marginBottom: 10 }}>
 								{getFieldDecorator('ruleset', {
 									rules: [{ required: true, message: 'Please choose a ruleset' }],
 									initialValue: "Default",
@@ -110,11 +181,12 @@ class NewJobForm extends React.Component {
 					</Row>
 					<Form.Item label="PDF Max Coverage Percentage">
 						{getFieldDecorator('maxCoverage', {
-							rules: [{ required: true, message: 'Please input your Password!' }],
+							rules: [{ required: true, message: 'Please input max coverage' }],
 						})(
 							<Row>
 								<Col span={12}>
 									<Slider
+										className={Style.formItemPaper}
 										min={1}
 										max={100}
 										onChange={this.onCoverageValChange}
@@ -137,45 +209,76 @@ class NewJobForm extends React.Component {
 
 					<h5>Paper Type</h5>
 					<div style={{ marginLeft: 30 }}>
-						<Form.Item label="Name:" {...paperFormItemLayout} style={{ marginBottom: 0, marginTop: 20 }}>
-							{getFieldDecorator('paperName')(
-								<Select className={Style.formItemPaper} onChange={this.onPaperNameChange} placeholder="Select a paper or fill out data manually">
-									<Option value="Paper A">Paper A</Option>
-									<Option value="Paper B">Paper B</Option>
-									<Option value="Paper C">Paper C</Option>
-								</Select>
-							)}
-						</Form.Item>
-						<Form.Item label="Surface:" {...paperFormItemLayout} style={{ marginBottom: 0 }}>
-							{getFieldDecorator('paperSurface', {
-								rules: [{ required: true, message: 'Please choose a surface type' }],
+						<Row gutter={12}>
+							<Col span={16}>
+								<Form.Item label="Name:" {...paperFormItemLayoutName} style={{ marginBottom: 0 }}>
+									{getFieldDecorator('productname')(
+										<Select className={Style.formItemPaper} onChange={this.onPaperNameChange} placeholder="Select a paper or fill out data manually">
+											{this.state.paperNameDropdown}
+										</Select>
+									)}
+								</Form.Item>
+							</Col>
+							<Col span={8}>
+								<Form.Item label="Mfr:" {...paperFormItemLayoutName} style={{ marginBottom: 0 }}>
+									{getFieldDecorator('manufacturer')(
+										<Input className={Style.formItemPaper} readOnly placeholder="(autofilled)" />
+									)}
+								</Form.Item>
+							</Col>
+						</Row>
+
+						<Form.Item label="Type:" {...paperFormItemLayout} style={{ marginBottom: 0 }}>
+							{getFieldDecorator('papertype', {
+								rules: [{ required: true, message: 'Please choose a paper type' }],
 							})(
 								<Radio.Group className={Style.formItemPaper} onChange={this.resetPaperName}>
-									<Radio.Button value="Coated">Coated</Radio.Button>
-									<Radio.Button value="Uncoated">Uncoated</Radio.Button>
+									{this.state.paperTypeRadio}
+								</Radio.Group>
+							)}
+						</Form.Item>
+						<Form.Item label="Sub-Type:" {...paperFormItemLayout} style={{ marginBottom: 0 }}>
+							{getFieldDecorator('papersubtype')(
+								<Radio.Group className={Style.formItemPaper} onChange={this.resetPaperName}>
+									{this.state.paperSubTypeRadio}
 								</Radio.Group>
 							)}
 						</Form.Item>
 						<Form.Item label="Weight:" {...paperFormItemLayout} style={{ marginBottom: 0 }}>
-							{getFieldDecorator('paperWeight', {
-								rules: [{ required: true, message: 'Please choose a weight' }],
+							{getFieldDecorator('weightclass', {
+								rules: [{ required: true, message: 'Please choose a weight class' }],
 							})(
 								<Radio.Group className={Style.formItemPaper} onChange={this.resetPaperName}>
-									<Radio.Button value="Light">Light</Radio.Button>
-									<Radio.Button value="Medium">Medium</Radio.Button>
-									<Radio.Button value="Heavy">Heavy</Radio.Button>
+									{this.state.paperWeightRadio}
 								</Radio.Group>
 							)}
 						</Form.Item>
 						<Form.Item label="Finish:" {...paperFormItemLayout} style={{ marginBottom: 0 }}>
-							{getFieldDecorator('paperFinish', {
+							{getFieldDecorator('finish', {
 								rules: [{ required: true, message: 'Please choose a finish' }],
 							})(
 								<Radio.Group className={Style.formItemPaper} onChange={this.resetPaperName}>
-									<Radio.Button value="Matte">Matte</Radio.Button>
-									<Radio.Button value="Gloss">Gloss</Radio.Button>
-									<Radio.Button value="Satin">Satin</Radio.Button>
-									<Radio.Button value="Silk">Silk</Radio.Button>
+									{this.state.paperFinishRadio}
+								</Radio.Group>
+							)}
+						</Form.Item>
+						<Form.Item label="Primer:" {...paperFormItemLayout} style={{ marginBottom: 0 }}>
+							{getFieldDecorator('useprimer', {
+								rules: [{ required: true, message: 'Please indicate primer use' }],
+							})(
+								<Radio.Group className={Style.formItemPaper} onChange={this.resetPaperName}>
+									<Radio.Button value="True">True</Radio.Button>
+									<Radio.Button value="False">False</Radio.Button>
+								</Radio.Group>
+							)}
+						</Form.Item>
+						<Form.Item label="Fixer:" {...paperFormItemLayout} style={{ marginBottom: 0 }}>
+							{getFieldDecorator('usefixer', {
+								rules: [{ required: true, message: 'Please indicate fixer use' }],
+							})(
+								<Radio.Group className={Style.formItemPaper} onChange={this.resetPaperName}>
+									<Radio.Button value="True">True</Radio.Button>
+									<Radio.Button value="False">False</Radio.Button>
 								</Radio.Group>
 							)}
 						</Form.Item>
