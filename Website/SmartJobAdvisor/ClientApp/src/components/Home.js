980bbaf217e6f6baee8c8dde540e562b60f37282
jonecole <jonecole@oregonstate.edu>
2020-02-18 17:02:00 -0800

    Updated server address to new instance's URL.

--- Website/SmartJobAdvisor/ClientApp/src/components/Home.js
+++ Website/SmartJobAdvisor/ClientApp/src/components/Home.js
@@ -4,7 +4,7 @@ import { Button, Icon } from 'antd';
 
 import Style from '../CSS/Home.module.css'
 
-export const ServerURL = "http://ec2-34-220-146-4.us-west-2.compute.amazonaws.com:8000/"
+export const ServerURL = "http://ec2-35-166-110-3.us-west-2.compute.amazonaws.com:8000/"
 
 export class Home extends Component {
     render() {
